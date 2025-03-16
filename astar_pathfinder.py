c9bf2845b118c901d57e922e96403e658266e51d
Joshua Defredes <mrjoshgm@gmail.com>
2025-03-16 18:16:01 +1100

    Fixed algorithm function error, added recreate_path function and added clear feature to clear board and nodes

--- astar_pathfinder.py
+++ astar_pathfinder.py
@@ -92,48 +92,56 @@ def h(p1, p2):
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
 
-def algorithm(draw, grid, start, end):
-    count = 0
-    open_set = PriorityQueue()
-    open_set.put((0, count, start))
-    came_from = {}
-    g_score = {node: float("inf") for row in grid for node in row}
-    g_score[start] = 0
-    f_score = {node: float("inf") for row in grid for node in row}
-    f_score[start] = h(start.get_pos(), end.get_pos())
-
-    open_set_hash = {start}
-
-    while not open_set.empty():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-        current = open_set.get()[2]
-        open_set_hash.remove(current)
-
-        if current == end:
-            return True
-
-        for neighbour in current.neighbours:
-            temp_g_score = g_score[current] + 1
-
-            if temp_g_score < g_score[neighbour]:
-                came_from[neighbour] = current
-                g_score[neighbour] = temp_g_score
-                f_score[neighbour] = temp_g_score + h(neighbour.get_pos(), end.get_pos())
-                if neighbour not in open_set_hash:
-                    count += 1
-                    open_set.put(f_score[neighbour], count, neighbour)
-                    open_set_hash.add(neighbour)
-                    neighbour.set_open()
-
+def recreate_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.set_path()
         draw()
 
-        if current != start:
-            current.set_closed()
+def algorithm(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {node: float("inf") for row in grid for node in row}
+	g_score[start] = 0
+	f_score = {node: float("inf") for row in grid for node in row}
+	f_score[start] = h(start.get_pos(), end.get_pos())
 
-    return False
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+
+		if current == end:
+			recreate_path(came_from, end, draw)
+			end.make_end()
+			return True
+
+		for neighbour in current.neighbours:
+			temp_g_score = g_score[current] + 1
+
+			if temp_g_score < g_score[neighbour]:
+				came_from[neighbour] = current
+				g_score[neighbour] = temp_g_score
+				f_score[neighbour] = temp_g_score + h(neighbour.get_pos(), end.get_pos())
+				if neighbour not in open_set_hash:
+					count += 1
+					open_set.put((f_score[neighbour], count, neighbour))
+					open_set_hash.add(neighbour)
+					neighbour.set_open()
+
+		draw()
+
+		if current != start:
+			current.set_closed()
+
+	return False
 
 def make_grid(rows, width):
     grid = []
@@ -182,16 +190,12 @@ def main(win, width):
     end = None
 
     run = True
-    started = False
     while run:
         draw(win, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-            
-            if started:
-                continue
 
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
@@ -219,12 +223,16 @@ def main(win, width):
                     end = None
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for node in row:
                             node.update_neighbours(grid)
 
                     algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
-    
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)    
     pygame.quit()
 main(WIN, WIN_WIDTH)
